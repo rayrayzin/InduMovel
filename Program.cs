5a5ba789bbf2b81823431ac694849f6c765df6c5
Aluno Dev <leticia.galdino3@portalsesisp.org.br>
2023-09-22 17:10:28 -0300

    repositories

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
 using InduMovel.Context;
+using InduMovel.Repositories;
+using InduMovel.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlite(builder.Configuration.GetConnectionString("DefaulConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaulConnection")));
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddTransient<IMovelRepository, MovelRepository>();
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-app.UseExceptionHandler("/Home/Error");
-app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
